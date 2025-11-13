e3355f1f021018c087dd950ab29ac9afba4b2d08
Sumit Attri <sumitattri165@gmail.com>
2025-11-13 17:14:26 +0530

    updated the provider for huggingface api

--- server/index.js
+++ server/index.js
@@ -89,7 +89,7 @@ app.get("/chat", async (req, res) => {
   const inputText = `${SYSTEM_PROMPT}\nUser: ${userQuery}\nAI:`;
 
   const chatResult = await hf.chatCompletion({
-    model: "HuggingFaceH4/zephyr-7b-beta",
+    model: "meta-llama/Llama-3-8b-instruct",
     provider: "hf-inference",
     messages: [
       { role: "system", content: SYSTEM_PROMPT },
